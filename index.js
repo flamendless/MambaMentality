8749187efe5e4a23934f12a16d21caf7febe6db3
flamendless <flamendless8@gmail.com>
2020-04-14 01:40:59 +0800

    Implemented favicon; Added view buttons (prototype)

--- index.js
+++ index.js
@@ -1,22 +1,79 @@
 $(document).ready(
 function()
 {
-	const images = {};
-	images.img_jumpshot = { original: $("#img_jumpshot").attr("src"), hovered: "assets/jumpshot_hovered.png"};
+	generate_mosaic();
 
-	// $(".banner").hover(
-	// 	function()
-	// 	{
-	// 		const img = $(this).find(".image");
-	// 		const id = img.attr("id");
-	// 		const obj = images[id];
-	// 		img.attr("src", obj.hovered);
-	// 	},
-	// 	function()
-	// 	{
-	// 		const img = $(this).find(".image");
-	// 		const id = img.attr("id");
-	// 		const obj = images[id];
-	// 		img.attr("src", obj.original);
-	// 	});
-})
+	const main = $("#main");
+	const mosaic = $("#mosaic");
+	const btn_minimize = $("#minimize");
+	const btn_maximize = $("#maximize");
+
+	const default_size = "repeat(4, 10vw)";
+	const max_size = "repeat(4, 1fr)";
+
+	btn_minimize.click(
+		function()
+		{
+			mosaic.css("grid-template-columns", default_size);
+			mosaic.addClass("center-grid");
+		});
+
+	btn_maximize.click(
+		function()
+		{
+			mosaic.css("grid-template-columns", max_size);
+			mosaic.removeClass("center-grid");
+		});
+});
+
+function generate_mosaic()
+{
+	const jumpshot = {
+		id: "jumpshot",
+		img: "assets/jumpshot.png",
+		alt: "Kobe on a jumpshot",
+		text: "The jumpshot",
+	};
+	const fadeaway = {
+		id: "fadeaway",
+		img: "assets/fadeaway.png",
+		alt: "Kobe on a fadeaway",
+		text: "The fadeaway",
+	};
+	const dunk = {
+		id: "dunk",
+		img: "assets/dunk.png",
+		alt: "Kobe on a dunk",
+		text: "The dunk",
+	};
+	const layup = {
+		id: "layup",
+		img: "assets/layup.png",
+		alt: "Kobe on a layup",
+		text: "The layup",
+	};
+	const defense = {
+		id: "defense",
+		img: "assets/defense.png",
+		alt: "Kobe on a defense",
+		text: "The defense",
+	};
+	const images = [
+		jumpshot, fadeaway, dunk, layup, defense
+	];
+	const mosaic = $("#mosaic");
+
+	for (let i = 0; i < images.length; i++)
+	{
+		const src = images[i];
+		const tile = $("<div></div>").addClass("mosaic-tile").addClass("flip").appendTo(mosaic);
+		const flip_inner = $("<div></div>").addClass("flip-inner").appendTo(tile);
+		const flip_front = $("<div></div>").addClass("flip-front").appendTo(flip_inner);
+		const img = $("<img>").attr("id", "img_" + src.id).attr("src", src.img).attr("alt", src.alt).appendTo(flip_front);
+		const flip_back = $("<div></div>").addClass("flip-back").appendTo(flip_inner);
+		const img_back = $("<img>").attr("id", "img_" + src.id).attr("src", src.img).attr("alt", src.alt).appendTo(flip_back);
+		const container = $("<div></div>").addClass("container-overlay").appendTo(flip_back);
+		const overlay = $("<div></div>").addClass("overlay").appendTo(container);
+		const text = $("<div></div>").addClass("overlay-text").text(src.text).appendTo(overlay);
+	}
+}
--- navbar.js
+++ navbar.js
@@ -9,13 +9,20 @@ function()
 
   const navbar = $("#navbar");
   const sticky = navbar.offset().top;
+  const duration_fade = 250;
 
   function scroll_navbar()
   {
     const scrolltop = $(window).scrollTop();
     if (scrolltop >= sticky)
+    {
+      navbar.fadeIn(duration_fade);
       navbar.addClass("sticky");
+    }
     else
+    {
+      navbar.fadeOut(duration_fade);
       navbar.removeClass("sticky");
+    }
   }
-})
+});
