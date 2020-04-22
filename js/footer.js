60c598a620e48d2cc6e12dd5134591305a61d3bb
flamendless <flamendless8@gmail.com>
2020-04-22 17:38:29 +0800

    Improved navbar

--- js/footer.js
+++ js/footer.js
@@ -1,30 +1,40 @@
 $(document).ready(
 function()
 {
+	$.get("./webpages/footer.html",
+		function(data)
+		{
+			$("#footer").html(data);
+			setup_effects();
+		});
+});
+
+function setup_effects()
+{
 	const p_yellow = "#fdb827";
 	const p_purple =  "#542583";
 
 	$.isvg(
-	function()
-	{
-		const svg = $("#svg_logo");
-		const path = svg.find("g path");
-		svg.attr("width", "64px");
-		path.css("fill", p_yellow);
-		path.css("stroke", p_purple);
+		function()
+		{
+			const svg = $("#svg_logo");
+			const path = svg.find("g path");
+			svg.attr("width", "64px");
+			path.css("fill", p_yellow);
+			path.css("stroke", p_purple);
 
-		svg.hover(
-			function()
-			{
-				path.css("fill", p_purple);
-				path.css("stroke", p_yellow);
-			});
+			svg.hover(
+				function()
+				{
+					path.css("fill", p_purple);
+					path.css("stroke", p_yellow);
+				});
 
-		svg.mouseleave(
-			function()
-			{
-				path.css("fill", p_yellow);
-				path.css("stroke", p_purple);
-			});
-	});
-});
+			svg.mouseleave(
+				function()
+				{
+					path.css("fill", p_yellow);
+					path.css("stroke", p_purple);
+				});
+		});
+}
--- js/navbar.js
+++ js/navbar.js
@@ -1,28 +1,68 @@
 $(document).ready(
 function()
 {
-  $(window).scroll(
-    function()
-    {
-      scroll_navbar();
-    });
-
-  const navbar = $("#navbar");
-  const sticky = navbar.offset().top;
-  const duration_fade = 250;
-
-  function scroll_navbar()
-  {
-    const scrolltop = $(window).scrollTop();
-    if (scrolltop >= sticky)
-    {
-      navbar.fadeIn(duration_fade);
-      navbar.addClass("sticky");
-    }
-    else
-    {
-      navbar.fadeOut(duration_fade);
-      navbar.removeClass("sticky");
-    }
-  }
+	$.get("./webpages/navbar.html",
+		function(data)
+		{
+			$("#navbar").html(data);
+			setup_sticky();
+			setup_svg();
+		});
 });
+
+function setup_sticky()
+{
+	$(window).scroll(
+		function()
+		{
+			scroll_navbar();
+		});
+
+	const navbar = $("#navbar");
+	const sticky = navbar.offset().top;
+	const duration_fade = 250;
+
+	function scroll_navbar()
+	{
+		const scrolltop = $(window).scrollTop();
+		if (scrolltop >= sticky)
+		{
+			navbar.fadeIn(duration_fade);
+			navbar.addClass("sticky");
+		}
+		else
+		{
+			navbar.fadeOut(duration_fade);
+			navbar.removeClass("sticky");
+		}
+	}
+}
+
+function setup_svg()
+{
+	const p_yellow = "#fdb827";
+	const p_purple =  "#542583";
+	$.isvg(
+		function()
+		{
+			const svg = $("#svg_logo_header");
+			const path = svg.find("g path");
+			svg.attr("height", "48px");
+			path.css("fill", p_yellow);
+			path.css("stroke", p_purple);
+
+			svg.hover(
+				function()
+				{
+					path.css("fill", p_purple);
+					path.css("stroke", p_yellow);
+				});
+
+			svg.mouseleave(
+				function()
+				{
+					path.css("fill", p_yellow);
+					path.css("stroke", p_purple);
+				});
+		});
+}
